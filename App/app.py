e5c82511013cdd789984e1608535aa8633439859
Juan Romero <juan.romero1201@gmail.com>
2020-08-19 13:40:42 -0500

    Cambio de Ruta

--- App/app.py
+++ App/app.py
@@ -148,8 +148,8 @@ def main():
         inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
         if len(inputs)>0:
             if int(inputs[0])==1: #opcion 1
-                loadCSVFile("Data/SmallMoviesDetailsCleaned.csv", lista)
-                loadCSVFile("Data/MoviesCastingRaw-small.csv", lista2)
+                loadCSVFile(r"C:\Users\Juan PC\Documents\Python Scripts\Lab\Data\SmallMoviesDetailsCleaned.csv", lista)
+                loadCSVFile(r"C:\Users\Juan PC\Documents\Python Scripts\Lab0_202020\Data\MoviesCastingRaw-small.csv", lista2)
                 print("Datos cargados, "+str(len(lista)+len(lista2))+" elementos cargados")
             elif int(inputs[0])==2: #opcion 2
                 if len(lista)==0: #obtener la longitud de la lista
